4be81c72422266e005e0b751d054f13133e8e88a
Lucas Henrique Torresin da Costa <lucas_hcosta@live.com>
2023-05-19 21:47:02 -0300

    Commit Aula 4

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -4,21 +4,12 @@ import { makeRedirectUri,  useAuthRequest } from 'expo-auth-session';
 import * as SecureStore from 'expo-secure-store'
 import {useRouter} from 'expo-router'
 
-
-import {
-  useFonts,
-  Roboto_400Regular,
-  Roboto_700Bold,
-} from '@expo-google-fonts/roboto'
-
-import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
-
-import blurBg from '../src/assets/bg-blur.png'
-import Stripes from '../src/assets/stripes.svg'
 import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
-import { styled } from 'nativewind'
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { api } from '../src/lib/api';
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree';
+import { Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { useFonts } from 'expo-font';
 
 const discovery = {
   authorizationEndpoint: 'https://github.com/login/oauth/authorize',
@@ -26,7 +17,6 @@ const discovery = {
   revocationEndpoint: 'https://github.com/settings/connections/applications/246f40681724d6c31a0f',
 };
 
-const StyledStripes = styled(Stripes)
 
 export default function App() {
   const router = useRouter()
@@ -68,49 +58,40 @@ export default function App() {
     }
   }, [response]);
 
-  
-  if (!hasLoadedFonts) {
-    return null
-  }
+
 
   return (
-    <ImageBackground
-      source={blurBg}
-      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
-      imageStyle={{ position: 'absolute', left: '-100%' }}
-    >
-      <StyledStripes className="absolute left-2" />
+    <View className="flex-1 items-center px-8 py-10">
+        <View className="flex-1 items-center justify-center gap-6">
+          <NLWLogo />
 
-      <View className="flex-1 items-center justify-center gap-6">
-        <NLWLogo />
+          <View className="space-y-2">
+            <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+              Sua cápsula do tempo
+            </Text>
+            <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+              Colecione momentos marcantes da sua jornada e compartilhe (se
+              quiser) com o mundo!
+            </Text>
+          </View>
 
-        <View className="space-y-2">
-          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
-            Sua cápsula do tempo
-          </Text>
-          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
-            Colecione momentos marcantes da sua jornada e compartilhe (se
-            quiser) com o mundo!
-          </Text>
+          <TouchableOpacity
+            activeOpacity={0.7}
+            className="rounded-full bg-green-500 px-5 py-2"
+            onPress={()=>signInWithGitHub()}
+          >
+            <Text className="font-alt text-sm uppercase text-black">
+              Cadastrar lembrança
+            </Text>
+          </TouchableOpacity>
         </View>
 
-        <TouchableOpacity
-          activeOpacity={0.7}
-          className="rounded-full bg-green-500 px-5 py-2"
-          onPress={()=>signInWithGitHub()}
-        >
-          <Text className="font-alt text-sm uppercase text-black">
-            Cadastrar lembrança
-          </Text>
-        </TouchableOpacity>
-      </View>
+        <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+          Feito com 💜 no NLW da Rocketseat
+        </Text>
 
-      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
-        Feito com 💜 no NLW da Rocketseat
-      </Text>
-
-      <StatusBar style="light" translucent />
-    </ImageBackground>
+        <StatusBar style="light" translucent />
+    </View>
   )
 }
 
